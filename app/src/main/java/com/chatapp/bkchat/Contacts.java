d978443fabe6f1fe2dd6ab79974b1d969e946095
ThanhHoang02 <101106973+ThanhHoang02@users.noreply.github.com>
2022-12-10 15:05:17 +0700

    Tim danh sach ban

---
+++ app/src/main/java/com/chatapp/bkchat/Contacts.java
@@ -0,0 +1,40 @@
+package com.chatapp.bkchat;
+
+public class Contacts
+{
+    public String username, description, image;
+
+    public Contacts(){
+
+    }
+
+    public Contacts(String username, String description, String image) {
+        this.username = username;
+        this.description = description;
+        this.image = image;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+}
--- app/src/main/java/com/chatapp/bkchat/FindFriendsActivity.java
+++ app/src/main/java/com/chatapp/bkchat/FindFriendsActivity.java
@@ -1,15 +1,31 @@
 package com.chatapp.bkchat;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
 import androidx.appcompat.widget.Toolbar;
+
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.squareup.picasso.Picasso;
+
+import org.w3c.dom.Text;
 
 public class FindFriendsActivity extends AppCompatActivity {
 
     private Toolbar mToolbar;
     private RecyclerView FindFriendsRecyclerList;
+    private DatabaseReference UsersRef;
 
 
 
@@ -18,6 +34,8 @@ public class FindFriendsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_find_friends);
 
+
+        UsersRef = FirebaseDatabase.getInstance().getReference().child("Users");
 
         FindFriendsRecyclerList =(RecyclerView) findViewById(R.id.find_friends_recycler_list);
         FindFriendsRecyclerList.setLayoutManager(new LinearLayoutManager(this));
@@ -30,4 +48,58 @@ public class FindFriendsActivity extends AppCompatActivity {
 
     }
 
+
+    @Override
+    protected void onStart()
+    {
+
+        super.onStart();
+
+        FirebaseRecyclerOptions<Contacts> options=
+                new FirebaseRecyclerOptions.Builder<Contacts>()
+                        .setQuery(UsersRef,Contacts.class)
+                        .build();
+
+        FirebaseRecyclerAdapter<Contacts, FindFriendViewHolder> adapter=
+                new FirebaseRecyclerAdapter<Contacts, FindFriendViewHolder>(options) {
+                    @Override
+                    protected void onBindViewHolder(@NonNull FindFriendViewHolder holder, int position, @NonNull Contacts model) {
+
+                        holder.userName.setText(model.getUsername());
+                        holder.userStatus.setText(model.getDescription());
+                        Picasso.get().load(model.getImage()).placeholder(R.drawable.image_user).into(holder.profileImage);
+                    }
+
+                    @NonNull
+                    @Override
+                    public FindFriendViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+                        View view= LayoutInflater.from(parent.getContext()) .inflate(R.layout.users_display_layout,parent,false);
+                        FindFriendViewHolder viewHolder = new FindFriendViewHolder(view);
+                        return viewHolder;
+                    }
+                };
+
+        FindFriendsRecyclerList.setAdapter(adapter);
+        adapter.startListening();
+
+
+    }
+    public static  class FindFriendViewHolder extends RecyclerView.ViewHolder
+    {
+
+        TextView userName, userStatus;
+        ImageView profileImage;
+        public FindFriendViewHolder(@NonNull View itemView)
+        {
+
+
+            super(itemView);
+
+            userName=itemView.findViewById(R.id.user_profile_name);
+            userStatus=itemView.findViewById(R.id.user_status);
+            profileImage=itemView.findViewById(R.id.users_profile_image);
+
+        }
+    }
 }
+
